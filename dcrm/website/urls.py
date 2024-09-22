f65412372ddd9550f38332238a6289534be55310
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-22 18:16:10 -0400

    Login Users

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -5,4 +5,6 @@ from . import views #12 - Import views.
 
 urlpatterns = [
     path('', views.home, name='home'), #11 - Create a new homepage.
+    path('login/', views.login_user, name='login'), #28 - login url to views.py
+    path('logout/', views.logout_user, name='logout'), #29 - logout url to views.py
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect #37 - import redirect function.
+from django.contrib.auth import authenticate, login, logout #25 - Import Django Authentication.
+from django.contrib import messages #26 - Flash successfull messages for login, logout, and registration.
+
+
 
 #13 - Define homepage view
 def home(request):
-    return render(request, 'home.html', {}) #14 - Pass an empty focus Python dict {}
+    
+    #34 - Check to see if logging in. If logging in = posting.
+    if request.method == 'POST':
+        username = request.POST['username'] #35 - Assign whatever is typed in form into variables created with username and password.
+        password = request.POST['password']
+        
+        #36 - Authenticate.
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been successfully logged in!')
+            return(redirect('home'))
+        else:
+            messages.success(request, 'There was an error logging in, please try again...')
+            return(redirect('home'))
+    else: #If not logging in, and just going to the page they are getting.
+        return render(request, 'home.html', {}) #14 - Pass an empty focus Python dict {}.
+
+#27 - Define login, logout views.
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
